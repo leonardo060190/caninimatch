ea9c028efbaaefdccced311502b67da4312b2360
leonardo060190 <leonardo-morruga@hotmail.com>
2023-12-11 20:29:09 -0300

    caninimatch com java

---
+++ src/main/java/com/example/caninimatch/CaninimatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.caninimatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaninimatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaninimatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/caninimatch/CaninimatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.caninimatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaninimatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
